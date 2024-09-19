88fe10d3811a0ee10ee81f2c7b4c7f570d223c67
lucaslmb971 <lucaslmb971@gmail.com>
2024-09-18 21:20:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Bienvenido a mi aplicacion de React!</h1>
+
+      <h2>Lista de cosas por hacer</h2>
+      <p>Aprender React</p>
+      <p>Practicar Vue</p>
+      <p>Construir proyectos increibles</p>
+    </>
+  );
+}
+
+export default App;
